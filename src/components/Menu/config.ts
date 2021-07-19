fb90917714706f995353d67283ccda974b892963
Wallace <wallacesip@gmail.com>
2021-07-19 14:04:24 -0300

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,87 @@
+import { MenuEntry } from '@zaigar-finance/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: "https://zaigar.finance",
+  },
+  {
+    label: 'Exchange',
+    icon: 'ExchangeIcon',
+    href: "/swap",
+  },  
+  {
+    label: 'Liquidity',
+    icon: 'LiquidityIcon',
+    href: "/pool",
+  },   
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://zaigar.finance/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://zaigar.finance/pools',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/zaigar-finance/',
+      },    
+      {
+        label: 'Price Chart',
+        href: 'https://goswapp-bsc.web.app/0x0288D3E353fE2299F11eA2c2e1696b4A648eCC07',
+      },      
+      {
+        label: 'Github',
+        href: 'https://github.com/zaigar-finance/',
+      },
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x0288D3E353fE2299F11eA2c2e1696b4A648eCC07',
+      },
+      {
+        label: 'Add to Metamask',
+        href: 'https://bit.ly/3sSDdrB',
+      },    
+    ],
+  },
+   {
+    label: 'zaigar.com',
+    icon:  'GroupsIcon',
+    href: 'https://zaigar.com',
+  },
+//   {
+//    label: 'Audit By Certik',
+//    icon: 'AuditIcon',
+//    href: 'https://certik.org/projects/zcore',
+//  },  
+   {
+    label: 'Docs',
+    icon: 'GitbookIcon',
+    href: 'https://docs.zcore.fi/',
+  },   
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x61C4819BeAE4761B36d84c14fb1978D6aA655712',
+  busdAddress: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
+  lpAddress: '0x24b87c29e907d6329ed8fd6d7b2ecb074089093e'
+}
+
+export default priceContracts
